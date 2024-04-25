11d4e85c7e82effd944a0c722b84d085168ea9fa
Jose Lucas <joselucas.dev@gmail.com>
2024-04-25 08:22:27 -0300

    feat:add services on controller

--- src/main/java/crud/com/example/crudspring/controllers/UserController.java
+++ src/main/java/crud/com/example/crudspring/controllers/UserController.java
@@ -1,16 +1,55 @@
 package crud.com.example.crudspring.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+import crud.com.example.crudspring.entity.UserEntity;
+import crud.com.example.crudspring.services.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 @RestController
 @RequestMapping("api/user")
 public class UserController {
 
+    private UserService _userService;
+
+UserController(UserService _userService){
+}
+
+
     @GetMapping()
-    public String UserGet(@RequestParam String param){
-        return "hello api" + " " + param;
+    public List<UserEntity> getUser(){
+        return this._userService.list();
     }
+
+    @PostMapping()
+    public List<UserEntity> createUser(@RequestBody UserEntity entity) {
+        return this._userService.createUser(entity);
+    }
+
+    @PutMapping()
+    public List<UserEntity> updateUser(@RequestBody UserEntity entity) {
+        
+        return this._userService.updateUser(entity);
+    }
+
+    @DeleteMapping("{id}")
+    public List<UserEntity> deleteUser(@PathVariable("id") Long id){
+        return this._userService.deleteUser(id);
+    }
+
+    @GetMapping("{id}")
+    public UserEntity findUserById(@PathVariable("id") Long id) {
+        return this._userService.findByIdUser(id);
+    }
+    
+
 }
