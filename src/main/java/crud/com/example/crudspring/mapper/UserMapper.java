bc084229d685b5a58aad796d6430d2be7010951d
Jose Lucas <joselucas.dev@gmail.com>
2024-05-08 08:52:22 -0300

    feat:add mapper java

---
+++ src/main/java/crud/com/example/crudspring/mapper/UserMapper.java
@@ -0,0 +1,39 @@
+package crud.com.example.crudspring.mapper;
+
+import org.springframework.stereotype.Component;
+
+import crud.com.example.crudspring.DTO.UserDTO;
+import crud.com.example.crudspring.entity.UserEntity;
+
+@Component
+public class UserMapper {
+
+    public UserDTO entityToDto(UserEntity userEntity){
+        if(userEntity == null){
+            return null;
+        };
+
+        return new UserDTO(userEntity.getId(),userEntity.getCpf(),userEntity.getAge(),userEntity.getName());
+
+    }
+    
+
+    public UserEntity dtoToEntity (UserDTO userDTO){
+        if(userDTO == null){
+            return null;
+        }
+        UserEntity user = new UserEntity();
+        if(userDTO.getId() != null){
+            user.setId(userDTO.getId());
+        };
+        user.setCpf(userDTO.getCpf());
+        user.setAge(userDTO.getAge());
+        user.setName(userDTO.getName());
+        
+        return user;
+    
+
+
+    }
+    
+}
