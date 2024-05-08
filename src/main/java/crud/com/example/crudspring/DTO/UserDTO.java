646c0f47518467166af08c0fc24f2283ff19267a
Jose Lucas <joselucas.dev@gmail.com>
2024-05-08 08:52:35 -0300

    feat:add UserDTO

---
+++ src/main/java/crud/com/example/crudspring/DTO/UserDTO.java
@@ -0,0 +1,58 @@
+package crud.com.example.crudspring.DTO;
+
+import crud.com.example.crudspring.entity.UserEntity;
+import jakarta.persistence.Id;
+
+public class UserDTO {
+    @Id
+    private Long id;
+    private String name;
+    private Integer age;
+    private String cpf;
+
+    public UserDTO(Long id,String name,Integer age, String cpf){
+    
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.cpf = cpf;
+    }
+
+    public UserDTO(UserEntity user){
+        id = user.getId();
+        name = user.getName();
+        cpf = user.getCpf();
+        age = user.getAge(); 
+    }
+
+    public Long getId(){
+        return this.id;
+    };
+
+    public void setId(Long id){
+        this.id = id;
+    };
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public Integer getAge(){
+        return this.age;
+    }
+    public void setAge(Integer age){
+        this.age = age;
+    }
+
+    public String getCpf(){
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+   }
