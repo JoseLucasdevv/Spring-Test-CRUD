4ee9bcc71a1f299da0f576d0728933fcf733de45
Jose Lucas <joselucas.dev@gmail.com>
2024-05-20 09:16:02 -0300

    feat:add test for post users succes and post users failure

--- src/test/java/crud/com/example/crudspring/CrudSpringApplicationTests.java
+++ src/test/java/crud/com/example/crudspring/CrudSpringApplicationTests.java
@@ -1,19 +1,47 @@
 package crud.com.example.crudspring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import crud.com.example.crudspring.DTO.UserDTO;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class CrudSpringApplicationTests {
+
+	@Autowired
+	private WebTestClient _webTestClient;
+
+	long id = 20;
+
+		UserDTO user = new UserDTO(id,"test",21,"123456");
 
 	@Test
 	void testCreateUserSuccess() {
+		
 
-	}
+			_webTestClient.post().uri("api/user").bodyValue(user).exchange().expectStatus().isOk().expectBody().jsonPath("$").isArray()
+			.jsonPath("$.length()").isEqualTo(1)
+			.jsonPath("$[0].name")
+			.isEqualTo(user.getName())
+			.jsonPath("$[0].age")
+			.isEqualTo(user.getAge())
+			.jsonPath("$[0].cpf")
+			.isEqualTo(user.getCpf());
+
+
+		}
 
 	@Test
 	void testCreateUserFailure() {
+		_webTestClient.post().uri("api/user").bodyValue(new UserDTO(id,"",10,"")).exchange().expectStatus().isBadRequest();
 
-	}
+
+
+		}
+
 
 }
