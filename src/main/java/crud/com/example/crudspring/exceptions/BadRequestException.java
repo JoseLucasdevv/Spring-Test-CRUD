d10b24bc34cb4cde17e98e41ef6de76941e01103
Jose Lucas <joselucas.dev@gmail.com>
2024-05-20 17:01:57 -0300

    feat:add BadRequestException

---
+++ src/main/java/crud/com/example/crudspring/exceptions/BadRequestException.java
@@ -0,0 +1,11 @@
+package crud.com.example.crudspring.exceptions;
+
+public class BadRequestException extends RuntimeException {
+    
+        public BadRequestException(String message){
+        super(message);
+       
+    };
+
+
+};
