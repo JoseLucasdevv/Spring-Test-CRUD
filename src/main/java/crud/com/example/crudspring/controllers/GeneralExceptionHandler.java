84de71b00d769a2422111ce28e3c06e34fb76618
Jose Lucas <joselucas.dev@gmail.com>
2024-05-20 21:57:58 -0300

    feat:add HandlevalidationExceptions

--- src/main/java/crud/com/example/crudspring/controllers/GeneralExceptionHandler.java
+++ src/main/java/crud/com/example/crudspring/controllers/GeneralExceptionHandler.java
@@ -1,16 +1,39 @@
 package crud.com.example.crudspring.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import crud.com.example.crudspring.exceptions.BadRequestException;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 
 @ControllerAdvice
 public class GeneralExceptionHandler {
 
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+
 @ExceptionHandler(BadRequestException.class)
  private ResponseEntity<Object> handleBadRequest(BadRequestException exception) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
     }
+
+ 
   }
